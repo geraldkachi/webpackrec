4530a5f5d237b0a0da96d5f75cd55a70018fc358
geraldkachi <fitzgeraldkachi@gmail.com>
2021-06-30 10:39:49 +0100

    more productive code

--- src/App.js
+++ src/App.js
@@ -1,38 +1,51 @@
-import React, { useState } from "react";
-import "./styles.css"
+import React, { useState, useEffect } from "react";
+import HashLoader from "react-spinners/HashLoader";
+import "./styles.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProjectForm from "./ProjectForm";
 
 const App = () => {
-    const [state, setState] = useState(false)
-    const [count, setCount] = useState(10)
+  const [state, setState] = useState(false);
+  const [count, setCount] = useState(10);
+
+  let [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setInterval(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <div style={bodyStyle} className="red"> 
-      <h1>Create React App Without CRA ☘️</h1>
-
-
-     <div> i love woman</div>
-
-      <button onClick={() => setState(state => !state)}>see me</button>
-      <div style={{backgroundColor: 'red'}}>{state} can you see me</div>
-
-      <div style={stylebtn}>
-          <button onClick={() => setCount(prev => prev + 1)}>+</button>
-          <div style={{margin: '12px'}}>{count}</div>
-          <button onClick={() => setCount(prev => prev - 1)}>-</button>
-      </div>
-    </div> 
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ProjectForm} />
+      </Switch>
+    </Router>
   );
 };
 
 export default App;
 
-
 const bodyStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-}
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
 const stylebtn = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+{
+  /* <button onClick={() => setState((state) => !state)}>see me</button>
+<div style={{ backgroundColor: "red" }}>{state} can you see me</div>
+
+<div style={stylebtn}>
+<button onClick={() => setCount((prev) => prev + 1)}>+</button>
+<div style={{ margin: "12px" }}>{count}</div>
+<button onClick={() => setCount((prev) => prev - 1)}>-</button>
+</div> */
 }
---
+++ src/ProjectForm.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import "./styles.css";
+
+const ProjectForm = () => {
+  return (
+    <div>
+      <div>
+        <h1>☘️ Order Form ☘️</h1>
+        <hr />
+      </div>
+      <div>
+        <Link>Express</Link>
+        <Link>Regular</Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectForm;
