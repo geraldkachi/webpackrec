f63d29909e47594587ce1d53209b779e5bad2d41
geraldkachi <fitzgeraldkachi@gmail.com>
2021-06-30 09:37:31 +0100

    code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles.css"
 
 const App = () => {
+    const [state, setState] = useState(false)
+    const [count, setCount] = useState(10)
   return (
-    <div style={{textAlign:'center'}}>
+    <div style={bodyStyle} className="red"> 
       <h1>Create React App Without CRA ☘️</h1>
-    </div>
+
+
+     <div> i love woman</div>
+
+      <button onClick={() => setState(state => !state)}>see me</button>
+      <div style={{backgroundColor: 'red'}}>{state} can you see me</div>
+
+      <div style={stylebtn}>
+          <button onClick={() => setCount(prev => prev + 1)}>+</button>
+          <div style={{margin: '12px'}}>{count}</div>
+          <button onClick={() => setCount(prev => prev - 1)}>-</button>
+      </div>
+    </div> 
   );
 };
 
 export default App;
+
+
+const bodyStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+}
+const stylebtn = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+}
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,14 @@ module.exports = {
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)?$/,
+        use: ["file-loader"],
+      },
     ],
   },
   resolve: {
@@ -19,9 +27,7 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"),
     filename: "bundle.js",
   },
-  plugins: [new webpack.HotModuleReplacementPlugin(
-    
-  )],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
     contentBase: path.resolve(__dirname, "./dist"),
     hot: true,
